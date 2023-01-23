bc7422331c7ba2fb3b15d8db9094ce247cee4a06
Камшилов Роман Сергеевич <kamshilovrs@mosreg.ru>
2023-01-23 22:41:23 +0300

    ready noChats page

--- src/pages/chat/mainSection/noChat/index.js
+++ src/pages/chat/mainSection/noChat/index.js
@@ -2,11 +2,11 @@ import tpl from './tpl.hbs';
 import './style.css';
 
 const data = {
-
+    text: 'Выберите чат чтобы отправить сообщение',
 };
 
 const noChat = () => {
-    return tpl();
+    return tpl(data);
 };
 
 export default noChat;
