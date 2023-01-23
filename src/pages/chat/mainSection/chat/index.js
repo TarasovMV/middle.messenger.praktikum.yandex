2930f6c667b8997a2a296899a96a4a6c9d7551da
Камшилов Роман Сергеевич <kamshilovrs@mosreg.ru>
2023-01-23 22:01:02 +0300

    ready chats page

--- src/pages/chat/mainSection/chat/index.js
+++ src/pages/chat/mainSection/chat/index.js
@@ -2,14 +2,23 @@ import  tpl from './tpl.hbs';
 import './style.css'
 import incomingMessage from "../../../../components/incomingMessage";
 import photoMessage from "../../../../components/photoMessage";
+import * as settings from '../../../../../static/settingsDots.svg';
+import * as clip from '../../../../../static/clip.svg';
+import * as sentArrow from '../../../../../static/sentArrow.svg';
+import sentMessage from "../../../../components/sentMessage";
+import inputMessage from "../../../../components/inputMessage";
 
 const data = {
     photo: '',
     name: 'Вадим',
-    settingsSvg: '',
+    settingsSvg: settings,
     date: '7 january',
+    clipSvg: clip,
+    sent: sentArrow,
     incomingMessage,
     photoMessage,
+    sentMessage,
+    inputMessage,
 };
 
 const chat = () => {
